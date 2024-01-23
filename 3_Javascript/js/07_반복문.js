138a699837a907cf5b455a28ba53c53fd4b1ad96
lhg010423 <lhg010423@gmail.com>
2024-01-23 17:49:26 +0900

    240123

---
+++ 3_Javascript/js/07_반복문.js
@@ -0,0 +1,112 @@
+/*
+for(let num = 1; num <= 5; num++) {
+    // 조건식이 true 인 경우 반복 수행할 코드;
+    console.log("통과!");
+}*/
+
+// 12345 출력
+function check1() {
+    let result = ""; // 빈 문자열
+    for(let num = 1; num <=5; num++) {
+        // console.log(num); 이것만 쓰면 5줄 생김
+
+        result += num;
+    }
+    console.log(result);
+}
+
+// 1부터 10까지 1씩 증가하며 출력
+function check2() {
+    for(let num = 1; num <= 10; num++) {
+        console.log(num);
+    } 
+}
+
+// 1부터 20까지 1씩 증가하며 출력
+function check3() {
+    for(let num = 1; num <= 20; num++) {
+        console.log(num);
+    }
+}
+
+// 5부터 15까지 1씩 증가하며 출력
+function check4() {
+    for(let num = 5; num <= 15; num++) {
+        console.log(num);
+    }
+}
+
+// 1부터 30까지 2씩 증가하며 출력
+function check5() {
+    for(let num = 1; num <= 30; num += 2) {
+        console.log(num);
+    }
+}
+
+// 1부터 10까지 모든 정수의 합
+function check6() {
+    let result = 0;
+    for(let num = 1; num <= 10; num++) {
+        result += num;
+    }
+    console.log(result);
+}
+
+// ------------------------------------------
+// 입력 받은 범위 내 모든 정수의 합 구하기
+function sumFn() {
+    // input에 작성된 값을 얻어와 저장, input 은 string 형식이다.
+    const start = Number(document.getElementById("inputNumber1-1").value);
+    const end = Number(document.getElementById("inputNumber1-2").value);
+
+    // 결과를 출력하기 위한 요소 얻어와 저장
+    const result1 = document.getElementById("result1");
+
+    // 합계를 정장할 변수
+    let sum = 0;
+
+    // start 부터 end 까지 1씩 증가하는 for 문
+    for(let num = start; num <= end; num++) {
+        sum += num; // num 값을 sum 에 누적
+    }
+
+    result1.innerText = sum;
+}
+
+// 입력 받은 범위 내에서 증감식 만큼 증가하며 모든 정수 출력하기
+function executeFn2() {
+    const start = Number(document.getElementById("inputNumber2-1").value);
+    const end = Number(document.getElementById("inputNumber2-2").value);
+    const val = Number(document.getElementById("inputNumber2-3").value);
+
+    // 결과 출력할 ul 요소
+    const ul = document.getElementById("result2"); // ul 태그
+
+    ul.innerHTML = ""; // 이전 ul에 작성된 내용을 모두 삭제
+    // start 부터 end 까지 val 씩 증가
+    for(let num = start; num <= end; num += val) {
+        ul.innerHTML += `<li>${num}</li>`;
+    }
+}
+
+// 요소.innerText = `<li>${num}</li>`;
+// -> 요소의 내용으로 문자열을 대입
+// (HTML 태그를 해석하지 않고 문자열 그대로 보여줌)
+
+// 요소.innerHTML = `<li>${num}</li>`;
+// -> HTML 태그를 해석해서 화면에 태그의 본 기능대로 출력
+
+
+
+// 입력 받은 단(2~9) 출력하기
+function executeFn3() {
+    const number = Number(document.getElementById("input3").value);
+
+    const ul = document.getElementById("result3");
+
+    ul.innerHTML = "";
+
+    for(let num = 1; num <= 9; num++) {
+        ul.innerHTML  += `<li>${number} x ${num} = ${number*num}</li>`;
+    }
+}
