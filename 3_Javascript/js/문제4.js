fe51e578e31009ea085b74fb0f91e0c964a35c1d
lhg010423 <lhg010423@gmail.com>
2024-01-31 17:44:53 +0900

    240131

---
+++ 3_Javascript/js/문제4.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById("btn");
+const checkBox = document.querySelector("fruit");
+
+
+btn.addEventListener("click", () => {
+    if(checkBox != null) {
+        
+    }
+});
